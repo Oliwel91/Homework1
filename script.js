adbc6d0204ee9c24bcdac2ea98f8a27f7fd1e46e
виктор паказанников <olwel@mail.ru>
2023-03-28 22:17:52 +0300

    Сделал 4 задания

---
+++ script.js
@@ -0,0 +1,76 @@
+// Задание 1
+
+// let password = 'qwer';
+// let input = String (prompt('Введите пароль'));
+// if (password === qwer ) {
+//     console.log ("Пароль введен верно");
+// } else{
+//     console.log ("Пароль введен неправильно");
+// }
+
+// Задание 2
+
+// let c = 5;
+// if (c > 0 && c < 10) {
+//     console.log ("Верно");
+// }else{
+//     console.log("Не верно")
+// }
+
+// let c = 0;
+// if (c > 0 && c < 10) {
+//     console.log ("Верно");
+// }else{
+//     console.log("Не верно")
+// }
+
+// let c = 10;
+// if (c > 0 && c < 10) {
+//     console.log ("Верно");
+// }else{
+//     console.log("Не верно")
+// }
+
+// let c = -3;
+// if (c > 0 && c < 10) {
+//     console.log ("Верно");
+// }else{
+//     console.log("Не верно")
+// }
+
+// let c = 2;
+// if (c > 0 && c < 10) {
+//     console.log ("Верно");
+// }else{
+//     console.log("Не верно")
+// }
+
+// Задание 3
+
+// let d = 20;
+// let e = 23;
+// if (d > 100 || e > 100) {
+//     console.log("Верно");
+    
+// } else {
+//     console.log("Не верно");
+
+    
+// }
+
+// Задание 4
+
+// let a = '2';
+// let b = '3';
+// alert(a + b);
+
+// Решение:
+
+// let a = '2';
+// let b = '3';
+// alert(0032 + 0033);
+// Тут не понял юникоды нужно записать в переменные или же как я сделал?
+
+// Задание 5
+
+
