4da8380fddace7a388cf9db8e552823363a8d246
виктор паказанников <olwel@mail.ru>
2023-04-01 15:59:35 +0300

    Сделал 3 задания

---
+++ scripts.js
@@ -0,0 +1,29 @@
+// 1.
+
+// let i = 1;
+// while (i < 6) {
+//     console.log(i);
+//     i++;
+    
+// }
+
+2.
+
+// let i = 1;
+// let n = 2;
+// while (i <= n) {
+//     alert('Привет !');
+//     i++;
+    
+// }
+
+3.
+// let i = 7;
+// while (i < 23) {
+//     console.log(i);
+//     i++;
+    
+// }
+
+4.
+
