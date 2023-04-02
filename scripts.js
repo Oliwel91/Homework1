6eb0cf2f4ac289f147ca0d21c2f4c6b964bc1c73
виктор паказанников <olwel@mail.ru>
2023-04-02 17:34:25 +0300

    Сделал 5 заданий

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@
 // while (i < 6) {
 //     console.log(i);
 //     i++;
-    
+
 // }
 
 2.
@@ -14,7 +14,7 @@
 // while (i <= n) {
 //     alert('Привет !');
 //     i++;
-    
+
 // }
 
 3.
@@ -22,8 +22,40 @@
 // while (i < 23) {
 //     console.log(i);
 //     i++;
-    
+
 // }
 
 4.
+
+
+// const obj = {
+//     Коля: 200,
+//     Вася: 300,
+//     Петя: 400,
+
+// }
+
+// for (const key in obj) {
+
+//     console.log(`${key} - зарплата ${obj[key]} долларов`);
+
+// }
+
+5.
+
+let n = 1000;
+let nam = 0;
+while (n >= 50) {
+    n /= 2;
+    nam++;
+}
+console.log(n);
+console.log(nam);
+
+
+
+
+
+
+
 
