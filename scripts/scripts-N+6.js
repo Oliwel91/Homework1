4c6c5ec5eae2f5ce108311cadf5e8c78ee4ccc4d
виктор паказанников <olwel@mail.ru>
2023-04-09 14:46:55 +0300

    13

--- scripts/scripts-N+6.js
+++ scripts/scripts-N+6.js
@@ -48,7 +48,79 @@
 // arr = arr.sort()
 // console.log(arr);
 
-7.
+// 7.
+
+// const arr = [9, 8, 7, 6, 5];
+
+// let result, checkNumber = prompt('Введите число от 1 до 10');
+
+// for(let i=0; i < arr.length; i++) {
+//     if(checkNumber == arr[i]) {
+//         result = 1;
+// 	}
+// }
+
+// if(result == 1) {
+// 	alert(`Число ${checkNumber} в массиве существует`);
+// } else { 
+// 	alert(`Числа ${checkNumber} в массиве не существует`);
+// }
+
+// 8.
+
+// let str = 'abcdef';
+
+// strChange = str.split('');
+
+// strChange = strChange.reverse();
+
+// str = strChange.join('');
+
+// console.log(str);
+
+// 9.
+
+// let arr1 = [1, 2, 3];
+
+// let arr2 = [4, 5, 6];
+
+// let arr = arr1.concat(arr2);
+
+// console.log(arr);
+
+// 10.
+
+// let arr = [3, 7, 3, 9, 6, 12, 5, 2, 8, 1];
+
+// for (let i = 0; i < arr.length - 1; i++) {
+	
+// 	console.log(arr[i] + arr[i + 1]);
+// }
+
+// 11.
+
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// const arrResult = arr.map(function (num) {
+//   return num * num
+// })
+
+// console.log(arrResult)
+
+// 12.
+
+// let getLengthWords = ['слово', '', 'слог', 'длинное предложение', 'буква'];
+// getLengthWords = getLengthWords.map(str => str.length);
+
+// console.log(getLengthWords);
+
+// 13.
+
+// let arr = [1, -2, 3, -4, 5, -6, 7, -8, 9];
+
+// arr = arr.filter(item => item <= 0);
+
+// console.log(arr);
 
 
 
