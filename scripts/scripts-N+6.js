9771d366b51d6da4e953838bdf9566af438d87e2
виктор паказанников <olwel@mail.ru>
2023-04-09 14:22:15 +0300

    7

--- scripts/scripts-N+6.js
+++ scripts/scripts-N+6.js
@@ -9,5 +9,48 @@
 // 	console.log(numbs[i]);
 // }
 
-2.
+// 2.
+
+// let arr = [1, 5, 4, 10, 0, 3];
+
+// console.log(arr.indexOf(4));
+
+// 3.
+
+// let arr = [1, 3, 5, 10, 20];
+// arr = arr.join(' ');
+// console.log(arr);
+
+// 4.
+
+// let arr = [];
+
+// for (let i = 0; i < 3; i++) {
+// 	arr[i] = [1]; 
+		
+// 	for (let b = 0; b < 2; b++) {
+// 		arr[i].push(1);
+// 	}
+// }
+	
+// console.log(arr);
+
+// 5.
+
+// let arr = [1, 1, 1];
+// arr.push  (2, 2, 2);
+// console.log(arr);
+
+// 6.
+
+// let arr = [9, 8, 7, 'a', 6, 5];
+// arr = arr.filter(item => item !== 'a');
+// arr = arr.sort()
+// console.log(arr);
+
+7.
+
+
+
+
 
