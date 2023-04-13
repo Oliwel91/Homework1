7468e351195adf4614d4488fe18053c741a2c1da
виктор паказанников <olwel@mail.ru>
2023-04-13 15:41:54 +0300

    8

--- scripts/script-N+7.js
+++ scripts/script-N+7.js
@@ -1,1 +1,73 @@
-1.
+// 1.
+
+// let newString = string.toUpperCase();
+
+// 2.
+// const animal =['Кошка', 'Кит', 'Комар', 'Носорог']
+
+// function str (arr, word) {
+//     return arr.map(element => element.toLowerCase()).filter(element => element.startsWith(word));
+
+// }
+
+// console.log(str(animal, 'ко'));
+
+// 3.
+
+// Math.floor(32.58884)
+// Math.ceil(32.58884)
+// Math.round(32.58884)
+
+// 4.
+
+//  console.log (Math.max(52, 53, 49, 77, 21, 32));
+//  console.log (Math.min(52, 53, 49, 77, 21, 32));
+
+//  5.
+
+//  function randomInteger (min, max) {
+//     let rand = min + Math.random() * (max + 1 - min);
+//     return Math.floor(rend);
+
+//  }
+//  console.log(randomInteger(1, 10));
+
+// 6.
+
+// let num = Number(prompt('Введите целое число'));
+
+
+
+// const arr = (num, min, max) => new Array(num).fill(0).map(el => Math.floor(Math.random() * (max - min + 1)) + min);
+
+// console.log(arr((Math.floor(num / 2)), 0, num));
+
+// 7.
+
+// let min = Number(prompt('Введите первое число'));
+// let max = Number(prompt('Введите второе число'));
+
+// let result = function (min,max){
+//   if (isNaN(min) || isNaN(max))  {
+//     console.log('Одно или оба значения не я вляются числом');
+    
+//   } else { let range = min + Math.random() * (max + 1 - min);
+//     console.log(Math.floor(range));
+//  }
+// }
+// result(min, max);
+
+8.
+
+
+
+
+
+
+
+
+
+
+
+
+
